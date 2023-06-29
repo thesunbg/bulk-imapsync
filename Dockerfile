dcaa9b542c9120f9ea32538b08f05f25e86289be
donv <donv@inet.vn>
2023-06-29 14:33:59 +0700

    add api

---
+++ Dockerfile
@@ -0,0 +1,76 @@
+FROM gilleslamiral/imapsync as img1
+# FROM python:3.9.10-alpine3.14 as img2
+USER root
+COPY Dockerfile imapsyn[c] prerequisites_imapsyn[c] /
+
+RUN set -xe && \
+  apt-get update \
+  && apt-get install -y \
+  libauthen-ntlm-perl \
+  libcgi-pm-perl \
+  libcrypt-openssl-rsa-perl \
+  libdata-uniqid-perl \
+  libencode-imaputf7-perl \
+  libfile-copy-recursive-perl \
+  libfile-tail-perl \
+  libio-compress-perl \
+  libio-socket-ssl-perl \
+  libio-socket-inet6-perl \
+  libio-tee-perl \
+  libhtml-parser-perl \
+  libjson-webtoken-perl \
+  libmail-imapclient-perl \
+  libparse-recdescent-perl \
+  libmodule-scandeps-perl \
+  libpar-packer-perl \
+  libreadonly-perl \
+  libregexp-common-perl \
+  libsys-meminfo-perl \
+  libterm-readkey-perl \
+  libtest-mockobject-perl \
+  libtest-pod-perl \
+  libunicode-string-perl \
+  liburi-perl  \
+  libwww-perl \
+  procps \
+  wget \
+  make \
+  cpanminus \
+  lsof \
+  ncat \
+  openssl \
+  ca-certificates \
+  && rm -rf /var/lib/apt/lists/* \
+  && cpanm IO::Socket::SSL
+
+# I added the IO::Socket::SSL update to avoid the annoying, confusing and useless warning
+# DEBUG: .../IO/Socket/SSL.pm:1177: global error: Undefined SSL object
+
+RUN set -xe \
+  && cd /usr/bin/ \
+  && pwd \
+  && wget -N --no-check-certificate https://imapsync.lamiral.info/imapsync \
+        https://imapsync.lamiral.info/prerequisites_imapsync \
+        https://raw.githubusercontent.com/google/gmail-oauth2-tools/master/python/oauth2.py \
+  && chmod +x imapsync oauth2.py
+
+USER nobody:nogroup
+
+ENV HOME /var/tmp/
+
+WORKDIR /var/tmp/
+
+STOPSIGNAL SIGINT
+
+USER root
+WORKDIR /srv
+RUN apt update
+RUN apt install -y software-properties-common
+RUN add-apt-repository ppa:deadsnakes/ppa
+RUN apt-get install -y python3.9
+RUN apt-get install -y pip
+RUN pip install --upgrade pip
+RUN pip install flask
+COPY . /srv
+ENV FLASK_APP=app
+CMD ["flask","--app", "api", "run", "--host", "0.0.0.0"]
---
+++ api.py
@@ -0,0 +1,102 @@
+from flask import Flask
+from flask import request, jsonify
+import os
+
+app = Flask(__name__)
+
+# Imap Sync 
+@app.route('/sync', methods = ['POST'])
+def sync():
+    from_host = request.json.get('from_host')
+    if(from_host == None):
+        return jsonify(status = 'error', message= 'from_host required'), 500
+    from_user = request.json.get('from_user')
+    if(from_user == None):
+        return jsonify(status = 'error', message= 'from_user required'), 500
+    from_password = request.json.get('from_password')
+    if(from_password == None):
+        return jsonify(status = 'error', message= 'from_password required'), 500
+
+    to_host = request.json.get('to_host')
+    if(to_host == None):
+        return jsonify(status = 'error', message= 'to_host required'), 500
+    to_user = request.json.get('to_user')
+    if(to_user == None):
+        return jsonify(status = 'error', message= 'to_user required'), 500
+    to_password = request.json.get('to_password')
+    if(to_password == None):
+        return jsonify(status = 'error', message= 'to_password required'), 500
+    command = "imapsync "
+    options = request.json.get('options')
+    if(options != None):
+        command += " " + options
+    command += f" --host1 {from_host} --user1 {from_user} --password1 '{from_password}'\
+                --host2 {to_host} --user2 {to_user} --password2 '{to_password}' --log &"
+    os.system(command)
+    return jsonify(status = "success", command = command), 200
+
+@app.route('/logs', methods = ['POST'])
+def logs():
+    from_user = request.json.get('from_user')
+    if(from_user == None):
+        return jsonify(status = 'error', message= 'from_user required'), 500
+    to_user = request.json.get('to_user')
+    if(to_user == None):
+        return jsonify(status = 'error', message= 'to_user required'), 500
+    try:
+        file_list = os.listdir("/var/tmp/uid_0/LOG_imapsync/")
+        response = []
+        fileJson = {}
+        fileIndex = 0
+        for file in file_list:
+            fileSplit = file.split('_')
+            fileJson["date"] = fileSplit[2] + '/' + fileSplit[1] + '/' + fileSplit[0]\
+                + ' ' + fileSplit[3] + ':'+ fileSplit[4] + ':'+ fileSplit[5]
+            fileJson["fromto"] = fileSplit[7] + ' ' + fileSplit[8]
+            if fileJson["fromto"] == from_user + ' ' + to_user + '.txt':
+                fileIndex = fileIndex + 1
+                response.append([fileIndex, fileJson["date"], file])
+        return jsonify(status = "success", file_list = response), 200
+    except:
+        return jsonify(status = "success", file_list = []), 200
+
+# View Log file 
+@app.route('/viewlog', methods = ['GET'])
+def viewlog():
+    try:
+        f = open("/var/tmp/uid_0/LOG_imapsync/" + request.args.get('file_name'))
+        return jsonify(status = "success", data = f.read()), 200
+    except:
+        return jsonify(status = "success", data = ''), 200
+
+# Delete Log 
+@app.route('/deletelog', methods = ['DELETE'])
+def deletelog():
+    try:
+        os.remove("/var/tmp/uid_0/LOG_imapsync/" + request.args.get('file_name'))
+        return jsonify(status = "success"), 200
+    except:
+        return jsonify(status = "success"), 200
+
+# Total logs file
+@app.route('/totallogs', methods = ['GET'])
+def totallogs():
+    try:
+        file_list = os.listdir("/var/tmp/uid_0/LOG_imapsync/")
+        return jsonify(status = "success", totalFiles = len(file_list)), 200
+    except:
+        return jsonify(status = "success", totalFiles = 0), 200
+
+# Delete all logs 
+@app.route('/deletealllogs', methods = ['DELETE'])
+def deletealllogs():
+    try:
+        file_list = os.listdir("/var/tmp/uid_0/LOG_imapsync/")
+        for file in file_list:
+            os.remove("/var/tmp/uid_0/LOG_imapsync/" + file)
+        return jsonify(status = "success"), 200
+    except:
+        return jsonify(status = "success"), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
