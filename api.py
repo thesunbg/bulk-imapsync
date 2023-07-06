2cf39f61844fd0b1f5a642fafd7901a67f5072cd
donv <donv@inet.vn>
2023-07-06 15:43:12 +0700

    add checklogin, viewslogdomainandresponsecode

--- api.py
+++ api.py
@@ -5,6 +5,36 @@ import signal
 
 app = Flask(__name__)
 
+responseCodes = [
+    'EX_OK',
+    'EX_USAGE',
+    'EX_NOINPUT',
+    'EX_UNAVAILABLE',
+    'EX_SOFTWARE',
+    'EXIT_CATCH_ALL',
+    'EXIT_BY_SIGNAL',
+    'EXIT_BY_FILE',
+    'EXIT_PID_FILE_ERROR',
+    'EXIT_CONNECTION_FAILURE',
+    'EXIT_TLS_FAILURE',
+    'EXIT_AUTHENTICATION_FAILURE',
+    'EXIT_SUBFOLDER1_NO_EXISTS',
+    'EXIT_WITH_ERRORS',
+    'EXIT_WITH_ERRORS_MAX',
+    'EXIT_OVERQUOTA',
+    'EXIT_ERR_APPEND',
+    'EXIT_ERR_FETCH',
+    'EXIT_ERR_CREATE',
+    'EXIT_ERR_SELECT',
+    'EXIT_TRANSFER_EXCEEDED',
+    'EXIT_ERR_APPEND_VIRUS',
+    'EXIT_TESTS_FAILED',
+    'EXIT_CONNECTION_FAILURE_HOST1',
+    'EXIT_CONNECTION_FAILURE_HOST2',
+    'EXIT_AUTHENTICATION_FAILURE_USER1',
+    'EXIT_AUTHENTICATION_FAILURE_USER2',
+]
+
 # Imap Sync 
 @app.route('/sync', methods = ['POST'])
 def sync():
@@ -36,6 +66,34 @@ def sync():
     os.system(command)
     return jsonify(status = "success", command = command), 200
 
+# Imap Sync 
+@app.route('/checklogin', methods = ['POST'])
+def checklogin():
+    from_host = request.json.get('from_host')
+    if(from_host == None):
+        return jsonify(status = 'error', message= 'from_host required'), 500
+    from_user = request.json.get('from_user')
+    if(from_user == None):
+        return jsonify(status = 'error', message= 'from_user required'), 500
+    from_password = request.json.get('from_password')
+    if(from_password == None):
+        return jsonify(status = 'error', message= 'from_password required'), 500
+
+    to_host = request.json.get('to_host')
+    if(to_host == None):
+        return jsonify(status = 'error', message= 'to_host required'), 500
+    to_user = request.json.get('to_user')
+    if(to_user == None):
+        return jsonify(status = 'error', message= 'to_user required'), 500
+    to_password = request.json.get('to_password')
+    if(to_password == None):
+        return jsonify(status = 'error', message= 'to_password required'), 500
+    command = "imapsync "
+    command += f" --host1 {from_host} --user1 {from_user} --password1 '{from_password}'\
+                --host2 {to_host} --user2 {to_user} --password2 '{to_password}' --log --justlogin &"
+    os.system(command)
+    return jsonify(status = "success", command = command), 200
+
 @app.route('/logs', methods = ['POST'])
 def logs():
     from_user = request.json.get('from_user')
@@ -61,12 +119,45 @@ def logs():
     except:
         return jsonify(status = "success", file_list = []), 200
 
+# Logs file of a domain and response code
+@app.route('/logsdomainandresponsecode', methods = ['POST'])
+def logsdomainandresponsecode():
+    domain = request.json.get('domain')
+    if(domain == None):
+        return jsonify(status = 'error', message= 'domain required'), 500
+    try:
+        file_list = os.listdir("/var/tmp/uid_0/LOG_imapsync/")
+        response = []
+        fileJson = {}
+        fileIndex = 0
+        for file in file_list:
+            fileSplit = file.split('_')
+            fileJson["date"] = fileSplit[2] + '/' + fileSplit[1] + '/' + fileSplit[0]\
+                + ' ' + fileSplit[3] + ':'+ fileSplit[4] + ':'+ fileSplit[5]
+            if '@' + domain in file:
+                fileIndex = fileIndex + 1
+                try:
+                    data = open("/var/tmp/uid_0/LOG_imapsync/" + file).read()
+                    for code in responseCodes:
+                        if(code in data):
+                            fileJson['responseCode'] = code
+                except:
+                    fileJson['responseCode'] = 'READ_ERR'
+                response.append([fileIndex, fileJson["date"], file, fileJson['responseCode']])
+        return jsonify(status = "success", file_list = response), 200
+    except:
+        return jsonify(status = "success", file_list = []), 200
+
 # View Log file 
 @app.route('/viewlog', methods = ['GET'])
 def viewlog():
     try:
-        f = open("/var/tmp/uid_0/LOG_imapsync/" + request.args.get('file_name'))
-        return jsonify(status = "success", data = f.read()), 200
+        data = open("/var/tmp/uid_0/LOG_imapsync/" + request.args.get('file_name')).read()
+        responseCode = ''
+        for code in responseCodes:
+            if(code in data):
+                responseCode = code
+        return jsonify(status = "success", data = data, responseCode= responseCode), 200
     except:
         return jsonify(status = "success", data = ''), 200
 
@@ -118,8 +209,8 @@ def killimapsyncprocess():
         os.kill(process_id, signal.SIGTERM)
         # os.system('kill -9 ' + process_id + ' && ps -ef | grep imapsync')
         return jsonify(status = "success", message = "killed"), 200
-    except:
-        return jsonify(status = "success"), 200
+    except Exception as e:
+        return jsonify(status = "error", message = e), 200
 
 def readcmd(cmd):
     ftmp = tempfile.NamedTemporaryFile(suffix='.out', prefix='tmp', delete=False)
